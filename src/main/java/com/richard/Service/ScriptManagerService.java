fad1bbeef343695d9b40cafd3b446dc79e04a1b6
Richard Hannah <richard.hannah1974@gmail.com>
2017-03-05 22:38:08 +0000

    added scriptmanagerservice and imple

--- src/main/java/com/richard/Service/ScriptManagerService.java
+++ src/main/java/com/richard/Service/ScriptManagerService.java
@@ -1,7 +1,13 @@
 package com.richard.Service;
 
+import com.richard.Domain.ScriptList;
+
 /**
  * Created by highl on 05/03/2017.
  */
 public interface ScriptManagerService {
+
+
+    ScriptList getScriptList();
+
 }
--- src/main/java/com/richard/Service/ScriptManagerServiceImpl.java
+++ src/main/java/com/richard/Service/ScriptManagerServiceImpl.java
@@ -2,8 +2,28 @@ package com.richard.Service;/**
  * Created by highl on 05/03/2017.
  */
 
+import com.richard.Dal.InternalDao;
+import com.richard.Domain.ScriptList;
+import org.springframework.stereotype.Service;
+
+import javax.inject.Inject;
+
 /**
  * created by highl
  */
-public class ScriptManagerServiceImpl {
+@Service
+public class ScriptManagerServiceImpl implements ScriptManagerService {
+
+    InternalDao internalDao;
+
+    @Inject
+    public ScriptManagerServiceImpl(InternalDao internalDao){
+        this.internalDao = internalDao;
+    }
+
+
+    @Override
+    public ScriptList getScriptList() {
+        return internalDao.fetchScriptList();
+    }
 }
