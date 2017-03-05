95f1b33ad86fac75075e470c2550c58f9a404f1a
Richard Hannah <richard.hannah1974@gmail.com>
2017-03-05 22:35:17 +0000

    added scripts endpoint

--- src/main/java/com/richard/Controller/TestController.java
+++ src/main/java/com/richard/Controller/TestController.java
@@ -4,11 +4,15 @@ package com.richard.Controller;
  * Created by highl on 02/03/2017.
  */
 
+import com.richard.Domain.ScriptList;
+import com.richard.Service.ScriptManagerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.inject.Inject;
 
 /**
  * created by highl
@@ -16,8 +20,26 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
 
+    private ScriptManagerService scriptManagerService;
+
+
+    @Inject
+    public TestController(ScriptManagerService scriptManagerService ){
+        this.scriptManagerService = scriptManagerService;
+    }
+
+
+
     @RequestMapping(value = "/hello", method = RequestMethod.GET)
     public ResponseEntity<String> hello(){
         return new ResponseEntity<String>("hello world", HttpStatus.OK);
+    }
+
+
+    @RequestMapping(value = "/scripts", method = RequestMethod.GET)
+    public ResponseEntity<ScriptList> getScripts(){
+
+        return new ResponseEntity<ScriptList>(scriptManagerService.getScriptList(),HttpStatus.OK);
+
     }
 }
